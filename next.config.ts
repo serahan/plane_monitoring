7db602339fe761645b4e4b55e59a41cb893d7007
serahan <seyrahan22@gmail.com>
2025-12-04 22:59:23 +0900

    feat : github action, pages 배포 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    output: "export", // 정적 사이트로 export
+    images: {
+        unoptimized: true, // GitHub Pages는 이미지 최적화 미지원
+    },
+    basePath: "/plane_monitoring", // 레포지토리 이름으로 변경
+    assetPrefix: "/plane_monitoring",
 };
 
 export default nextConfig;
